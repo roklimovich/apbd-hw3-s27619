4d0475149ca2686320b97c0c1a342c29949a3048
Roman Klimovich <ro.klimovich@gmail.com>
2026-03-26 16:02:36 +0100

    feat: method to find students with enrollments was implemented

--- LinqConsoleLab.EN/Exercises/LinqExercises.cs
+++ LinqConsoleLab.EN/Exercises/LinqExercises.cs
@@ -198,7 +198,13 @@ public sealed class LinqExercises
     /// </summary>
     public IEnumerable<string> Task11_JoinStudentsWithEnrollments()
     {
-        throw NotImplemented(nameof(Task11_JoinStudentsWithEnrollments));
+        return UniversityData.Students
+            .Join(
+                UniversityData.Enrollments,
+                s => s.Id,
+                e => e.StudentId,
+                (s, e) => $"{s.FirstName} {s.LastName} | Enrollment Date: {e.EnrollmentDate:yyyy-MM-dd}"
+            );
     }
 
     /// <summary>
